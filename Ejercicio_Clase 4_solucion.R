c299920c3575bca2df9ac201b9aa398025f7b535
julian-corbo <juliancorbo00@gmail.com>
2024-04-25 15:34:54 -0300

    Variables construidas, falta educacion y chequear con el INE

--- Ejercicio_Clase 4_solucion.R
+++ Ejercicio_Clase 4_solucion.R
@@ -1,106 +1,230 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                 Introducción al Análisis de bases de datos                     #
+#                    Operacionalización: Construcción de NBI                     #
+#                                  Clases 4 y 5                                  #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+### Carga de paquetes ###
+
 library(rio)
 library(osfr)
 library(here)
 library(tidyverse)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-### Carga de censo ###
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-#df  <- rio::import("Auxiliares/Clase 4/Ejercicio/Base unificada Viv_Hog_Pers.sav")
-### Lo guardo en RData ### 
-#save(df, file = "Auxiliares/Clase 4/Ejercicio/Censo 2011.RData")
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+### Carga de base Censo 2011 (INE) ###
+
+## Opción 1: cargar archivo desde ruta
+
+df  <- rio::import("MI RUTA/Censo 2011.Rdata") # Base competa (descargar de web INE u OSF)
+df  <- rio::import("MI RUTA/Censo2011_muestra.Rdata") # Base reducida solo para ejercicios (descargar de EVA)
+
+
+## Opción 2: descargar desde web OSF - UMAD
 
 if (!dir.exists(here::here("Censo"))) {dir.create(here::here("Censo"))}
 
 osfr::osf_retrieve_file("4jczy") %>% 
   osfr::osf_download(here("Censo"), conflicts = "overwrite")
+
 df <- rio::import(here::here("Censo", "Censo 2011.Rdata"))
 
 
-#Me quedo solo con las viviendas particulares
-df <- subset (df, VIVVO01<9)
-
-#Identifico hogar en cada vivienda
-
-df <- df %>% mutate(NUMERO = paste(ID_VIVIENDA, HOGID))
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-
-### Generación de nuevas variables ###
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 
-###### NBI - Vivienda decorosa
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+### Transformaciones generales ###
+
+# Me quedo solo con las viviendas particulares
+
+df <- subset(df, VIVVO01<9)
+
+# Genero una variable de identificación del hogar en cada vivienda
+
+df <-df %>% mutate(NUMERO = paste(ID_VIVIENDA, HOGID))
 
 
-## Materialidad vivienda
-df <- df %>% dplyr::mutate(NBI_materialidad = ifelse(VIVDV02==5 | VIVDV01==6 | VIVDV03==4, 1, 
-                                              ifelse(VIVDV02==0, 99, 0)))
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 
-## Espacio habitable - Hacinamiento
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+### CONSTRUCCIÓN DE VARIABLES DE NBI ###
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-#Genero cantidad de personas en el hogar
-df <- df %>% mutate(unos = 1) 
-df <- df %>% group_by(NUMERO) %>% mutate(nint = sum(unos)) %>% as.data.frame()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~ Vivienda decorosa ~~~ #
 
-df <- df %>% dplyr::mutate(NBI_HACINAMIENTO  = ifelse(nint/HOGHD00>2, 1,
-                                               ifelse(HOGHD00==88, 99, 0)))
-
-## Espacio apropiado para cocinar
-df <- df %>% dplyr::mutate(NBI_cocina = ifelse(HOGSC01==3, 1, 
-                                        ifelse(HOGSC01==8,99,0)))
-
-
-df <- df %>% dplyr::mutate(NBI_VIVIENDA = ifelse(NBI_HACINAMIENTO==1|
-                                                 NBI_cocina==1|
-                                                 NBI_materialidad==1, 1,
-                                          ifelse(NBI_HACINAMIENTO==99|
-                                                 NBI_cocina==99|
-                                                 NBI_materialidad==99, 99,0)))
+# Materialidad vivienda
+# El hogar habita una vivienda con techos o paredes construidas
+# predominantemente con materiales de desecho, o piso de tierra sin
+# piso ni contrapiso.
 
 
-###### NBI - Abastecimiento de agua potable
+df <- df %>% 
+  dplyr::mutate(NBI_materialidad = ifelse(VIVDV02 == 5 | 
+                                          VIVDV01 == 6 | 
+                                          VIVDV03 == 4, 1, 
+                                   ifelse(VIVDV02 == 0, 99, 0)))
 
-df <- df %>% dplyr::mutate(NBI_agua = ifelse(VIVDV05==0,99,
-                                      ifelse(VIVDV06!=1 | VIVDV05>=3, 1,0)))
-
-
-###### NBI - Servicio higiénico de calidad
-
-df <- df %>% dplyr::mutate(NBI_servhigien = ifelse(HOGSH01==8,99,
-                                            ifelse(HOGSH01==3|HOGSH02==2|HOGSH03>2, 1,0)))
+prop.table(table(df$NBI_materialidad))*100
 
 
-###### NBI - Energía eléctrica
-df <- df %>% dplyr::mutate(NBI_energia = ifelse(VIVDV07==6, 1, 
-                                         ifelse(VIVDV07==0,99,0)))
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Espacio habitable (Hacinamiento)
+# Umbral: Más de dos miembros del hogar por habitación en la vivienda
+# (excluyendo baño y cocina).
+
+df <- df %>% 
+  mutate(unos = 1) # Variable que vale 1 para toda las personas
+df <- df %>% 
+  group_by(NUMERO) %>% 
+  mutate(nint = sum(unos)) %>% 
+  as.data.frame() # Cuenta cantidad de personas por hogar
+
+df <- df %>% 
+  dplyr::mutate(NBI_hacinamiento  = ifelse(nint / HOGHD00 > 2, 1,  # Cantidad de habitaciones residenciales mayor estricto que dos
+                                               ifelse(HOGHD00 == 88, 
+                                                      99, 
+                                                      0)))
+
+prop.table(table(df$NBI_hacinamiento))*100
 
 
-###### NBI - Artefactos básicos de cofnort
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Espacio apropiado para cocinar
+# Umbral: El hogar habita una vivienda que no cuenta con un espacio para
+# cocinar con canilla y pileta.
 
-df <- df %>% dplyr::mutate(NBI_artefactos = ifelse(HOGCE01==8,99,
-                                            ifelse(HOGCA01==9 | 
-                                                   HOGCE03==2 | 
-                                                  (HOGCE01==2&HOGCE02==2), 1, 0)))
+df <- df %>% dplyr::mutate(NBI_cocina = ifelse(HOGSC01 == 3, 
+                                               1, 
+                                        ifelse(HOGSC01 == 8,
+                                               99,
+                                               0)))
 
-
-###### NBI - Educación
-
-df <- df %>% dplyr::mutate(EDUC = ifelse(PERNA01>3&PERNA01<18 &  
-                                         PERED01>2 &
-                                        (PERED03_1_r<6 | 
-                                        ((PERED03_1_r==6|PERED03_1_r==7)&PERED04_r==2)),1,0))
+prop.table(table(df$NBI_cocina))*100
 
 
+df <- df %>% dplyr::mutate(NBI_vivienda = ifelse(NBI_hacinamiento == 1 |
+                                                 NBI_cocina       == 1 |
+                                                 NBI_materialidad == 1, 
+                                                 1,
+                                          ifelse(NBI_hacinamiento == 99 |
+                                                 NBI_cocina       == 99 |
+                                                 NBI_materialidad == 99, 
+                                                 99,
+                                                 0)))
 
-df <- df %>% group_by(NUMERO) %>% mutate(NBI_EDUCACION11 = max(EDUC)) %>% as.data.frame()
+prop.table(table(df$NBI_vivienda))*100
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~ Abastecimiento de agua potable ~~~ #
+# Umbral: El agua no llega por cañería dentro de la vivienda que habita el
+# hogar, o su origen no es red general o pozo surgente protegido.
+
+df <- df %>% dplyr::mutate(NBI_agua = ifelse(VIVDV05 == 0,
+                                             99,
+                                      ifelse(VIVDV06 != 1 | VIVDV05 >= 3, 
+                                             1, 
+                                             0)))
+
+prop.table(table(df$NBI_agua))*100
 
 
-###### NBI Total
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~ Servicio higiénico de calidad ~~~ #
+# El hogar no accede a baño de uso exclusivo o la evacuación del
+# servicio sanitario no es a través de la red general, fosa séptica o pozo
+# negro.
 
-df <- df %>% dplyr::mutate(NBI_2011= ifelse(NBI_vivienda11==1 | NBI_agua11==1 | NBI_servhigien11==1 | NBI_energia11==1 | NBI_artefactos11==1 | NBI_educacion11==1, 1, 0))
-df <- df %>% dplyr::mutate(NBI_cant= NBI_vivienda11 + NBI_agua11 + NBI_servhigien11 + NBI_energia11 + NBI_artefactos11 + NBI_educacion11)
+df <- df %>% dplyr::mutate(NBI_servhigien = ifelse(HOGSH01 == 8, 
+                                                   99,
+                                            ifelse(HOGSH01 == 3 |
+                                                     HOGSH02 == 2 |
+                                                     HOGSH03 > 2, 
+                                                   1,
+                                                   0)))
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+prop.table(table(df$NBI_servhigien))*100
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~ Energía eléctrica ~~~ #
+# El hogar no cuenta con energía eléctrica en la vivienda que habita.
+
+df <- df %>% dplyr::mutate(NBI_energia = ifelse(VIVDV07 == 6, 
+                                                1, 
+                                         ifelse(VIVDV07 == 0,
+                                                99,
+                                                0)))
+
+prop.table(table(df$NBI_energia))*100
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~ Artefactos básicos de cofnort ~~~ #
+# El hogar no cuenta con ningún medio para calefaccionar la vivienda que habita. 
+# El hogar no cuenta con heladera o freezer.
+# El hogar no posee calefón, termofón, caldereta o calentador instantáneo.
+
+df <- df %>% dplyr::mutate(NBI_artefactos = ifelse(HOGCE01 == 8, 99,
+                                            ifelse(HOGCA01 == 9 | 
+                                                   HOGCE03 == 2 | 
+                                                  (HOGCE01 == 2 &
+                                                     HOGCE02 == 2), 1, 0)))
+
+prop.table(table(df$NBI_artefactos))*100
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~ Educación ~~~ #
+# Al menos un integrante del hogar con edad comprendida entre los
+# 4 y los 17 años no se encuentra asistiendo a un centro educativo
+# formal, no habiendo finalizado enseñanza secundaria.
+
+
+df <- df %>% 
+  dplyr::mutate(tramoed = ifelse(PERNA01 > 3 & PERNA01 < 18, 1, 0),
+                asiste  = ifelse((PERED00 >= 1 & PERED00 <= 3) | 
+                                             PERED01 >= 1 & PERED01 <= 2, 1, 0))
+
+df <- df %>% 
+  dplyr::mutate(finaliza = ifelse(asiste == 0 & 
+                                    PERED03_1_r >= 6 & 
+                                    PERED04_r == 1, 1, 0))
+                           
+df <- df %>% 
+  dplyr::mutate(nbiedupers = ifelse(tramoed == 1 & 
+                                      asiste == 0 & 
+                                      finaliza == 0, 1, 0))
+
+                                   
+df <- df %>% group_by(NUMERO) %>% 
+  mutate(NBI_educacion = max(nbiedupers))
+
+prop.table(table(df$NBI_educacion))*100
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~ Total NBI ~~~ #
+
+df <- df %>% 
+  dplyr::mutate(NBI_2011= ifelse(NBI_vivienda   == 1 | 
+                                 NBI_agua       == 1 | 
+                                 NBI_servhigien == 1 |
+                                 NBI_energia    == 1 | 
+                                 NBI_artefactos == 1 | 
+                                 NBI_educacion  == 1, 
+                                 1, 
+                                 ifelse(NBI_vivienda     == 99 | 
+                                          NBI_agua       == 99 | 
+                                          NBI_servhigien == 99 |
+                                          NBI_energia    == 99 | 
+                                          NBI_artefactos == 99 | 
+                                          NBI_educacion  == 99, 99, 0)))
+
+prop.table(table(df$NBI_2011))*100
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
--- ejercicio_clase4.R
+++ ejercicio_clase4.R
@@ -2,5 +2,121 @@
 install.packages("rio")
 
 library(rio)
+library(tidyverse)
 
-df <- rio::import("Users/Julian/Desktop/JULIAN/R/FCS/Base unificada Viv_Hog_Pers.dbf" )
+#df <- rio::import("C:/Users/Julian/Desktop/JULIAN/R/FCS/Base unificada Viv_Hog_Pers.dbf")
+
+df <- rio::import("C:/Users/Julian/Desktop/JULIAN/R/FCS/bbdd/Censo2011_muestra.RData")
+
+df <- subset(df, VIVVO01<9)
+
+
+# Segun material de construccion de piso techo y paredes. 
+df <- df %>% 
+  dplyr::mutate(NBI_materialidad = ifelse(
+                                      VIVDV01 == 6 | VIVDV02 == 5 | VIVDV03 == 4,
+                                      1,
+                                      ifelse(VIVDV02 == 0, 99, 0)))
+
+table(df$NBI_MAT,df$NBI_materialidad)
+table(df$NBI_materialidad)
+
+# Espacio apropiado para cocina 
+
+df <- df %>% 
+  dplyr::mutate(NBI_cocina = ifelse(
+                                HOGSC01 == 3, 1,
+                                ifelse(HOGSC01 == 8, 99, 0)))
+
+
+table(df$NBI_cocina)
+
+
+# hacinamiento 
+
+
+
+df <- df %>%
+  dplyr::mutate(NBI_hacinamiento = case_when(HOGPR01/HOGHD00 > 2 ~ 1, TRUE ~ 0)) #Me falta 99 
+
+table(df$NBI_cocina)
+
+df <- df %>% 
+  dplyr::mutate(NBI_vivienda_dec = ifelse(NBI_materialidad == 1| NBI_hacinamiento == 1 | NBI_cocina == 1, 1, 0))
+
+
+table(df$NBI_HAC, df$NBI_hacinamiento)
+
+#agua (No se si esta bien)
+
+df <- df %>% 
+  dplyr::mutate(NBI_agua = ifelse(
+                              VIVDV05 >= 3 | VIVDV06 >3, 1,
+                              ifelse(VIVDV05 == 8 | VIVDV05 == 0 | VIVDV06 == 0, 99, 0)))
+  
+
+round(prop.table(table(df$NBI_agua))*100, 1)
+
+#higiene
+
+df <- df %>% 
+  dplyr::mutate(NBI_higiene = ifelse(HOGSH03 == 8 | HOGSH02 == 8, 99,
+                                     ifelse(HOGSH03 > 2 | HOGSH02 !=1, 1, 0 )))
+
+round(prop.table(table(df$NBI_higiene))*100, 1)
+
+
+df <- df %>% 
+  dplyr::mutate(NBI_electrico = ifelse(VIVDV07 == 6, 1,
+                                       ifelse(VIVDV07 == 8 | VIVDV07 == 0, 99, 0)))
+                                            
+
+round(prop.table(table(df$NBI_electrico))*100, 1)    
+
+
+df <- df %>% 
+  dplyr::mutate(NBI_calefaccion = ifelse(HOGCA01 == 9, 1, 
+                                      ifelse(HOGCA01 == 8 | HOGCA01 == 0 , 99, 0)))
+
+                                      
+
+round(prop.table(table(df$NBI_calefaccion))*100, 1)  
+
+
+df <- df %>% 
+  dplyr::mutate(NBI_alimentos = ifelse(HOGCE03 == 2, 1, 
+                                       ifelse(HOGCE03 == 8 | HOGCE03 == 0, 99, 0)))
+
+round(prop.table(table(df$NBI_alimentos))*100, 1)  
+
+
+
+df <- df %>% 
+  dplyr::mutate(NBI_cal_banio = ifelse(HOGCE02 == 2 & HOGCE01 == 2, 1, 
+                                       ifelse(HOGCE02 == 8 | HOGCE02 == 0 |
+                                              HOGCE01 == 8 | HOGCE01 == 0, 99, 
+                                              0)))
+
+
+round(prop.table(table(df$NBI_cal_banio))*100, 1)
+
+df <- df %>% 
+  dplyr::mutate(NBI_artefactos2 = dplyr::case_when(NBI_alimentos == 1 |
+                                                   NBI_calefaccion == 1 |
+                                                   NBI_cal_banio == 1 ~1,
+                                                   NBI_alimentos == 99 |
+                                                   NBI_calefaccion == 99 |
+                                                   NBI_cal_banio == 99 ~ 99,
+                                                   TRUE ~ 0))
+
+round(prop.table(table(df$NBI_artefactos2))*100, 1)
+
+
+#EDUCACION
+
+
+
+
+
+
+
